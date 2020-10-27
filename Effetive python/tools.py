cf0408fa3e708c0eb4666c956b57e1ffe5e07d13
Saumen95 <saumenroy323@gmail.com>
2020-10-27 18:19:09 +0600

    tools

--- Effetive python/tools.py
+++ Effetive python/tools.py
@@ -1,18 +1,23 @@
-class Tools:
+class Tool:
 
-    def __init__(self, name, weight):
+    def __init__(self,name,weight):
         self.name = name
         self.weight = weight
 
     def __repr__(self):
-        return f'Tool({self.name !r}, {self.weight})'
+        return f'Tool({self.name!r}, {self.weight})'
 
-    tools = [
-        Tools('Leveller', 3.5),
-        Tools('Hammer', 3),
-        Tools('Screwdriver', 2)
-    ]
 
-    # Tools.sort will not work
-    print('Unsorted', repr(tools))
-    tools.sort(key=lambda x:x.name)
+tools = [
+    Tool('Leveller', 3.5),
+    Tool('Hammer', 2.5),
+    Tool('Screwdriver', 1)
+]
+
+print('Unsorted:', repr(tools))
+tools.sort(key=lambda x: x.name)
+print('\nsorted: ', tools)
+
+tools.sort(key=lambda x: x.weight)
+print('\nBy weight', tools)
+

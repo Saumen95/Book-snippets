e2751b9692bd5a0609be9dba8209c482b5e8ddfa
Saumen95 <saumenroy323@gmail.com>
2020-10-27 18:29:57 +0600

    tools

--- Effetive python/tools.py
+++ Effetive python/tools.py
@@ -14,10 +14,20 @@ tools = [
     Tool('Screwdriver', 1)
 ]
 
+power_tools = [
+    Tool('Drill', 0.5),
+    Tool('Saw', 0.75),
+    Tool('Jackhammer', 5),
+    Tool('Sander', 4)
+]
+
 print('Unsorted:', repr(tools))
 tools.sort(key=lambda x: x.name)
 print('\nsorted: ', tools)
 
 tools.sort(key=lambda x: x.weight)
 print('\nBy weight', tools)
+
+power_tools.sort(key=lambda x: x.weight, reverse=True)
+print('\nBy weight', power_tools)
 

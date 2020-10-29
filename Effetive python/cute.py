08876e9209e32c55c19c26633fea19de8c0c3232
Saumen95 <saumenroy323@gmail.com>
2020-10-29 11:08:34 +0600

    cute

--- Effetive python/cute.py
+++ Effetive python/cute.py
@@ -7,7 +7,7 @@ votes = {
 
 def populate_ranks(votes, ranks):
     names =list(votes.keys())
-    names.sort(votes.get, reverse=True)
+    names.sort(key=votes.get, reverse=True)
     for i,name in enumerate(names,1):
         ranks[name] = i
 

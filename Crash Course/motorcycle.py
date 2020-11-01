e76e829b1d7657ea4b25a0309cb96bc5a73597fc
Saumen95 <saumenroy323@gmail.com>
2020-11-01 13:45:27 +0600

    imp list method

---
+++ Crash Course/motorcycle.py
@@ -0,0 +1,9 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop(1)
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+motorcycles.insert(2, 'Ducati')  # insert at 3rd pos
+motorcycles.remove('honda')  # insert
+numbers = [1, 3, 56, 45, 34, 25, 67, 87]
+numbers.sort(reverse=True)
+numbers.count(25)
+print(len(numbers))

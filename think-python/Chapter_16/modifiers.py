09d081ca662ca2cbd73c846991f1e9ef4eb1007e
Saumen95 <saumenroy323@gmail.com>
2020-11-03 17:42:48 +0600

    time script

---
+++ think-python/Chapter_16/modifiers.py
@@ -0,0 +1,10 @@
+def increment(time, seconds):
+
+    time.second += seconds
+    if time.second >= 60:
+        time.second -= 60
+        time.minute += 1
+
+    if time.minute >= 60:
+        time.minute -= 60
+        time.hour += 1
